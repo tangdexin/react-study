9c83ea633e4e2d1ce6010e80b3c3f6b58618d0de
hittdx <940165364@qq.com>
2017-08-31 16:41:32 +0800

    2

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                hello world!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
